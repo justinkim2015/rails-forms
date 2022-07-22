af4271cd45593084e4200a961398fc4c02a43ec7
justinkim2015 <justinkim2015@gmail.com>
2022-07-22 12:16:49 +0900

    Set up basic backend

---
+++ re-former/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def new
+    # @user = User.new
+  end
+
+  def create
+    # if @user.save
+    #   #do something
+    # else 
+    #   #error message
+    #   render :new, status: :unprocessable_entity
+    # end
+  end
+end
