64e4aecd6e3a9b89ad8c97717d092505494e6476
justinkim2015 <justinkim2015@gmail.com>
2022-07-23 09:06:40 +0900

    Create basic HTML form

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -4,11 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    # if @user.save
-    #   #do something
-    # else 
-    #   #error message
-    #   render :new, status: :unprocessable_entity
-    # end
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
